a838198e144c0dfe9fb0aa182c235e1589e6a619
myasinK <yasinul@gmail.com>
2018-08-13 01:43:29 -0700

    Commit first C# drill

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcom to Acme Accounting Systems \nRemember, we\'re \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
